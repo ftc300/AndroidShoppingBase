7e05e0ab62cfbc9466119b19fa1a35e0a7f7678a
shenjianli <cqtddt@163.com>
2016-06-20 10:53:49 +0800

    commit card files

---
+++ src/com/shoping/mall/study/cardslidepanel/CardDataItem.java
@@ -0,0 +1,13 @@
+package com.shoping.mall.study.cardslidepanel;
+
+/**
+ * 卡片数据装载对象
+ *
+ * @author xmuSistone
+ */
+public class CardDataItem {
+    String imagePath;
+    String userName;
+    int likeNum;
+    int imageNum;
+}
