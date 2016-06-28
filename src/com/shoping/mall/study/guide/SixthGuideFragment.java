b0d826d846eac00627783e947c5abac39161fc2c
shenjianli <cqtddt@163.com>
2016-06-28 14:01:54 +0800

    commit guidle fragment and activity

---
+++ src/com/shoping/mall/study/guide/SixthGuideFragment.java
@@ -0,0 +1,48 @@
+package com.shoping.mall.study.guide;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.shoping.mall.R;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@xisue.com
+ * Date: 7/14/14
+ * Time: 11:10 PM
+ * Desc: ThirdGuideFragment
+ */
+public class SixthGuideFragment extends BaseGuideFragment {
+
+    View mLayoutLogo;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_guide_sixth, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mLayoutLogo = view.findViewById(R.id.guide_item_1);
+
+        Animation anim = AnimationUtils.loadAnimation(getActivity(), R.anim.splash_guide_logo);
+        mLayoutLogo.startAnimation(anim);
+    }
+
+    @Override
+    public int[] getChildViewIds() {
+        return new int[]{};
+    }
+
+    @Override
+    public int getRootViewId() {
+        return R.id.layout_guide_sixth;
+    }
+}
