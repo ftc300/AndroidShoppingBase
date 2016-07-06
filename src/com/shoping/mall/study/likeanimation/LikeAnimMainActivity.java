fd5d7adc88fd629ba2fd18b750e992bba37f4f43
shenjianli <cqtddt@163.com>
2016-07-06 21:00:41 +0800

    commit main activity and button view

---
+++ src/com/shoping/mall/study/likeanimation/LikeAnimMainActivity.java
@@ -0,0 +1,15 @@
+package com.shoping.mall.study.likeanimation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.shoping.mall.R;
+
+public class LikeAnimMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.likeanim_activity_main);
+    }
+}
