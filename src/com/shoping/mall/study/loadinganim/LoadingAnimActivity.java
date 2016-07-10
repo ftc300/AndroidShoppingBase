500a03eb54b33fbc4397dae2b4ec4f92d47de1b5
shenjianli <cqtddt@163.com>
2016-07-10 21:18:58 +0800

    commit main files

---
+++ src/com/shoping/mall/study/loadinganim/LoadingAnimActivity.java
@@ -0,0 +1,15 @@
+package com.shoping.mall.study.loadinganim;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.shoping.mall.R;
+
+public class LoadingAnimActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.loadinganim_activity_main);
+  }
+}
