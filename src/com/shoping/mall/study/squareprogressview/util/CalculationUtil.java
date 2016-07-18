1577c418b8fc3f324a81b362f80838e69e736f66
shenjianli <cqtddt@163.com>
2016-07-18 19:37:35 +0800

    commit files of square progress view

---
+++ src/com/shoping/mall/study/squareprogressview/util/CalculationUtil.java
@@ -0,0 +1,12 @@
+package com.shoping.mall.study.squareprogressview.util;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+public class CalculationUtil {
+
+	public static int convertDpToPx(float dp, Context context) {
+		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+				context.getResources().getDisplayMetrics());
+	}
+}
